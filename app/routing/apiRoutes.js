69210e4f04bd9b4a0d0209d998af978c6ec1430d
Casey Carroll <caseydean8@gmail.com>
2019-08-17 14:21:03 -0400

    server.js connecting to apiRoutes and htmlRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@
 
 //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-const friendData = require('../data.friendData')
+const friendData = require('../data/friends')
 
 module.exports = app => {
     app.get('/api/friends',
@@ -10,6 +10,6 @@ module.exports = app => {
         res.json(friendData)
     }))
     app.post('/api/friends', ((req, res) => {
-        // insert logic here
+        console.log("insert logic here")
     }))
-}
+} 
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+// npms
 const express = require('express')
-const path = require('path')
+// const path = require('path')
 
+// creating express server
 const app = express()
+
+// set a port
 const PORT = process.env.PORT || 3000
 
 // set up express to handle data parsing
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
+// router
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 // set port listener
 app.listen(PORT, () => {
