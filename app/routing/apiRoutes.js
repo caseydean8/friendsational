4af04b9f50abcc21f2df6c57040d2ab1175347a9
Casey Carroll <caseydean8@gmail.com>
2019-08-18 16:47:20 -0400

    can find differences in values between 2 arrays.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,11 +16,37 @@ module.exports = app => {
         //loop through friendData objects and compare most recent friend score vs previous scores, subtracting differences at correlating index positions, creating a new array with differences summed.
         // create compare data array 
         let compareData = []
+        let count = -1
         for (let i = 0; i < friendData.length; i++) {
             let score = friendData[i].scores
+            count++
             // console.log(score)
             compareData.push(score)
-            console.log(compareData)
+            // console.log(`array of survey answer values from friend data ${compareData}`)
+            console.log(`friend data list is ${count} long`)
         }
+        console.log(compareData)
+        arrayCompare(compareData[0], compareData[1])
+        // let difference =[]
+        // for (let i = 0; i != compareData.length; i++) {
+        //     for (let j = 0; j != compareData[i].length; j++) {
+        //     difference.push(compareData[i][j] - compareData[count][j])
+        //     console.log(difference)
+        //     if (difference.length === 10) {
+        //         break
+        //     }
+        //     }
+        // }
+        // console.log(difference)
     }))
 } 
+
+function arrayCompare(a, b) {
+    cArray = []
+    for (let i = 0; i < 10; i++) {
+       c = Math.abs(a[i] - b[i])
+       cArray.push(c)
+       console.log(cArray)
+    }
+    console.log(cArray)
+}
